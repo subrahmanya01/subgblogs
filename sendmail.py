33ce91cdac30ca783cb788f1b1d6b6c45e5074e5
subrahmanya01 <60250884+subrahmanya01@users.noreply.github.com>
2023-02-12 12:57:08 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -7,11 +7,11 @@ with open("config.json","r") as c:
 def send_mail(code,first_name,reciever_mail,verifie_=False):
         server = smtplib.SMTP('smtp.gmail.com',587)
         server.starttls()
-        server.login("subgblogs1@gmail.com","ytoupilgeagztmvy")
+        server.login("yourgamil","yourpassword")
         if verifie_:
           print("Verification mail sent")
-          server.sendmail("subgblogs1@gmail.com",reciever_mail,msg="Subject:Verification Code\n\n"+msg['emailmsg'].format(first_name,code))
+          server.sendmail("yourgmail",reciever_mail,msg="Subject:Verification Code\n\n"+msg['emailmsg'].format(first_name,code))
         else:
           print("Reset mail sent")
-          server.sendmail("subgblogs1@gmail.com",reciever_mail,msg="Subject:Password Reset\n\n"+msg['resetmsg'].format(first_name,code))
+          server.sendmail("yourgmail",reciever_mail,msg="Subject:Password Reset\n\n"+msg['resetmsg'].format(first_name,code))
         print("Mail sent")
